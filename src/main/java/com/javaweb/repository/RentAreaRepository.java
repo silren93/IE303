f34a427ed0e02da5ad1490bc5de115b80d8660e3
Trần Lâm Ngọc Khanh <me123lac@gmail.com>
2025-05-18 14:52:58 +0700

    update enities

--- src/main/java/com/javaweb/repository/RentAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.repository;
-
-import java.util.List;
-
-import com.javaweb.repository.entity.RentAreaEntity;
-
-public interface RentAreaRepository {
-    List<RentAreaEntity> getValueByBuildingId(Long id);
-}
